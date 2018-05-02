a08715d901db46d5b7959b80dc6c4573d1a86df3
Stephanie <s.a.james5@gmail.com>
2018-05-02 18:27:17 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,6 +1,8 @@
 def position_taken?(board, index)
   if board[index] == " "  
     return false
+  elsif board[index] == ""
+    return false 
   else board && index == "X" || "O"   
     return true
     end
