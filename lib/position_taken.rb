ab25e18f7bc0fcf049eaec4b85b2326a991e7948
Stephanie <s.a.james5@gmail.com>
2018-05-02 18:49:57 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,4 +1,5 @@
 def position_taken?(board, index)
-   (board[index] == " " || board[index] == "" || board[index] == nil)
+   if (board[index] == " " || board[index] == "" || board[index] == nil)
+     
   end
   
